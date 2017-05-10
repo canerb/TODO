5594de898dd872e6951a249c28b1062a67d5473f
Caner Bozkurt <caner.bozkurt.94@googlemail.com>
2017-05-10 19:26:05 +0200

    Added functionality to show imprint.

---
+++ resources/views/imprint.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.master')
+
+@section('content')
+	<div class="row">
+		<div class="col-sm-12">
+			<p><h1>Authors</h1></p>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-sm-12 col-md-4">
+			<div class="panel panel-default text-center">
+				<div class="panel-heading">
+					<img class="img-circle img-responsive center-block" src="http://placehold.it/200">
+				</div>
+				<div class="panel-body"><h3>Caner</h3></div>
+			</div>
+		</div>
+		<div class="col-sm-12 col-md-4">
+			<div class="panel panel-default text-center">
+				<div class="panel-heading">
+					<img class="img-circle img-responsive center-block" src="http://placehold.it/200">
+				</div>
+				<div class="panel-body"><h3>Cihan</h3></div>
+			</div>
+		</div>
+		<div class="col-sm-12 col-md-4">
+			<div class="panel panel-default text-center">
+				<div class="panel-heading">
+					<img class="img-circle img-responsive center-block" src="http://placehold.it/200">
+				</div>
+				<div class="panel-body"><h3>Khaled</h3></div>
+			</div>
+		</div>
+	</div>
+@endsection
